2c60ee2d7c9e52813eccd51318af0f6aba4552db
Mike Foster <i.am.mike.foster@gmail.com>
2026-02-16 13:25:58 +0000

    parse test function name

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-pub fn identify(_src: &str) -> &str{
-    ""
+use ruff_python_parser::{ParseError, parse_module};
+
+pub fn identify(src: &str) -> Result<String, ParseError>{
+    let stmts = parse_module(src)?.into_suite();
+    let testfn = stmts.first().unwrap();
+    let testname: String = testfn.as_function_def_stmt().unwrap().name.to_string();
+    Ok(testname)
 }
 
 #[cfg(test)]
@@ -8,10 +13,10 @@ mod tests {
 
     #[test]
     fn one_test() {
-        let src = "
-        def test_passes():
-            assert True
-        ";
-        assert_eq!("test_passes", identify(src));
+        let src = r"
+def test_passes():
+    assert True
+";
+        assert_eq!("test_passes", identify(src).unwrap());
     }
 }
