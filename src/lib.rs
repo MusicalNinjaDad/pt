5ee86abb4a8f32d36cf68f3ae89141298f75bfc8
Mike Foster <i.am.mike.foster@gmail.com>
2026-02-16 13:40:39 +0000

    require test function to start with `test_`

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,15 @@ pub fn identify(src: &str) -> Result<String, ParseError> {
     let stmts = parse_module(src)?.into_suite();
     let testfn = stmts
         .iter()
-        .find(|stmt| stmt.is_function_def_stmt())
+        .find(|stmt| {
+            stmt.is_function_def_stmt()
+                && stmt
+                    .as_function_def_stmt()
+                    .unwrap()
+                    .name
+                    .as_str()
+                    .starts_with("test_")
+        })
         .unwrap();
     let testname: String = testfn.as_function_def_stmt().unwrap().name.to_string();
     Ok(testname)
@@ -27,6 +35,20 @@ def test_passes():
     fn test_not_first() {
         let src = r"
 import foo
+
+def test_passes():
+    assert True
+";
+        assert_eq!("test_passes", identify(src).unwrap());
+    }
+
+    #[test]
+    fn test_follows_other_functions() {
+        let src = r"
+import foo
+
+def not_a_test():
+    pass
 
 def test_passes():
     assert True
