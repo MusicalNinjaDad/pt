4c0c26476d909ea6e9f4780f4d50a95b6b67342d
Mike Foster <i.am.mike.foster@gmail.com>
2026-02-16 13:37:46 +0000

    get first function def

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,10 @@ use ruff_python_parser::{ParseError, parse_module};
 
 pub fn identify(src: &str) -> Result<String, ParseError> {
     let stmts = parse_module(src)?.into_suite();
-    let testfn = stmts.first().unwrap();
+    let testfn = stmts
+        .iter()
+        .find(|stmt| stmt.is_function_def_stmt())
+        .unwrap();
     let testname: String = testfn.as_function_def_stmt().unwrap().name.to_string();
     Ok(testname)
 }
@@ -14,6 +17,17 @@ mod tests {
     #[test]
     fn one_test() {
         let src = r"
+def test_passes():
+    assert True
+";
+        assert_eq!("test_passes", identify(src).unwrap());
+    }
+
+    #[test]
+    fn test_not_first() {
+        let src = r"
+import foo
+
 def test_passes():
     assert True
 ";
