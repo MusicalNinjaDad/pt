04f3a1d6f229988facea526223b5447f335d6d3b
Mike Foster <i.am.mike.foster@gmail.com>
2026-02-18 09:23:00 +0000

    compare generated with fixture

--- tests/test_generation.rs
+++ tests/test_generation.rs
@@ -1,1 +1,19 @@
+use std::{fs, future, path::Path};
+
 use pt::generate;
+
+#[test]
+fn test_generation() {
+    let src = r"
+import pathlib
+
+def test_fails():
+    assert False
+
+def test_passes():
+    assert True
+";
+    let expected_runner = fs::read_to_string("./tests/fixtures/test.py").unwrap();
+    let runner = generate(src.to_string()).unwrap();
+    assert_eq!(expected_runner, runner);
+}
