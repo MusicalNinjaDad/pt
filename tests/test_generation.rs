3774688a40651d61af325e9cb78269e9e9d8ef0b
Mike Foster <i.am.mike.foster@gmail.com>
2026-02-20 17:11:52 +0000

    move fixture

--- tests/test_generation.rs
+++ tests/test_generation.rs
@@ -3,7 +3,7 @@ use std::fs;
 use pt::{TestSuite, Traceback};
 
 #[test]
-fn test_generation() {
+fn basic() {
     let src = r"import pathlib
 
 
@@ -14,12 +14,12 @@ def test_fails():
 def test_passes():
     assert True
 ";
-    let expected_runner = fs::read_to_string("./tests/fixtures/test.py").unwrap();
+    let expected_runner = fs::read_to_string("./tests/fixtures/basic/test.py").unwrap();
     let mut suite: TestSuite = src.try_into().unwrap();
     let runner = suite.runner("UID");
     assert_eq!(expected_runner, runner);
-    let stdout = fs::read_to_string("./tests/fixtures/stdout.out").unwrap();
-    let stderr = fs::read_to_string("./tests/fixtures/stderr.out").unwrap();
+    let stdout = fs::read_to_string("./tests/fixtures/basic/stdout.out").unwrap();
+    let stderr = fs::read_to_string("./tests/fixtures/basic/stderr.out").unwrap();
     suite.update_status("UID", &stdout, &stderr);
     assert!(matches!(
         &suite.tests["test_passes"].status,
