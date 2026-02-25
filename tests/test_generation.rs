f13ad6225ba347a9c1718c996dacbaa2166520fc
Mike Foster <i.am.mike.foster@gmail.com>
2026-02-25 15:39:15 +0000

    check status of third test

--- tests/test_generation.rs
+++ tests/test_generation.rs
@@ -33,7 +33,7 @@ fn basic() {
 fn complex() {
     let id = "UID";
     let fixtures = PathBuf::from("./tests/fixtures/complex");
-        let src = fs::read_to_string(fixtures.join("src.py")).unwrap();
+    let src = fs::read_to_string(fixtures.join("src.py")).unwrap();
     let mut suite = TestSuite::try_from(src.as_str()).unwrap();
     assert_eq!(3, suite.tests.len());
     let expected_runner = fs::read_to_string(fixtures.join("run.py")).unwrap();
@@ -45,13 +45,15 @@ fn complex() {
         &suite.tests["test_passes"].status,
         pt::TestStatus::Pass
     ));
-    let expect_tb: Traceback = fs::read_to_string(fixtures.join("test_fails.tb"))
-        .unwrap()
-        .as_str()
-        .into();
-    let tf_status = &suite.tests["test_fails"].status;
-    assert!(
-        matches!(tf_status, pt::TestStatus::Fail(tb) if tb == &expect_tb),
-        "{tf_status:?}"
-    );
+    for failed_test in ["test_fails", "test_seven_is_six"] {
+        let expect_tb: Traceback = fs::read_to_string(fixtures.join(failed_test).with_added_extension("tb"))
+            .unwrap()
+            .as_str()
+            .into();
+        let tf_status = &suite.tests[failed_test].status;
+        assert!(
+            matches!(tf_status, pt::TestStatus::Fail(tb) if tb == &expect_tb),
+            "{tf_status:?}"
+        );
+    }
 }
