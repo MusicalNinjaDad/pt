b268f413da4ad4fbf46b3a7b95c1d817f2e14880
Mike Foster <i.am.mike.foster@gmail.com>
2026-03-04 16:54:39 +0000

    bench pt

---
+++ benches/bench.rs
@@ -0,0 +1,14 @@
+use std::path::PathBuf;
+
+use assert_cmd::cargo::cargo_bin_cmd;
+use criterion::{Criterion, criterion_group, criterion_main};
+
+fn benchmarks(c: &mut Criterion) {
+    let pysrc = PathBuf::from("./tests/fixtures/basic/src.py");
+    let mut pt_cmd = cargo_bin_cmd!("pt");
+    pt_cmd.arg(pysrc.as_os_str());
+    c.bench_function("pt", |b| b.iter(|| pt_cmd.output()));
+}
+
+criterion_group!(basic, benchmarks);
+criterion_main!(basic);
