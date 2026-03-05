6150b0b588d3c7fce5b779af4826cdb4b2d58279
Mike Foster <i.am.mike.foster@gmail.com>
2026-03-05 07:20:41 +0000

    run a proper macro-bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,15 +1,35 @@
-use std::{path::PathBuf, process::Command};
+use std::{path::PathBuf, process::Command, time::Instant};
 
 use criterion::{Criterion, criterion_group, criterion_main};
 use escargot::CargoBuild;
 
 fn benchmarks(c: &mut Criterion) {
     let pysrc = PathBuf::from("./tests/fixtures/basic/src.py");
-    let mut pt_cmd = CargoBuild::new().bin("pt").current_release().run().unwrap().command();
-    let mut pytest_cmd = Command::new("pytest");
-    pytest_cmd.arg(pysrc.as_os_str());
-    c.bench_function("pt", |b| b.iter(|| pt_cmd.output()));
-    c.bench_function("pytest", |b| b.iter(|| pt_cmd.output()));
+    let pt_bin = CargoBuild::new()
+        .bin("pt")
+        .current_release()
+        .run()
+        .unwrap()
+        .path()
+        .to_owned();
+    c.bench_function("pt", |b| {
+        b.iter_custom(|iters| {
+            let start = Instant::now();
+            for _ in 0..iters {
+                let _ = Command::new(&pt_bin).arg(&pysrc).output().unwrap();
+            }
+            start.elapsed()
+        })
+    });
+    c.bench_function("pytest", |b| {
+        b.iter_custom(|iters| {
+            let start = Instant::now();
+            for _ in 0..iters {
+                let _ = Command::new("pytest").arg(&pysrc).output().unwrap();
+            }
+            start.elapsed()
+        })
+    });
 }
 
 criterion_group!(basic, benchmarks);
