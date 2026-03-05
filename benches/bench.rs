a634666d1473ef6805d037c13ae613d5e2b4f5ff
Mike Foster <i.am.mike.foster@gmail.com>
2026-03-05 06:03:24 +0000

    also bench pytest

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,13 +1,16 @@
 use std::path::PathBuf;
 
-use assert_cmd::cargo::cargo_bin_cmd;
+use assert_cmd::{Command, cargo::cargo_bin_cmd};
 use criterion::{Criterion, criterion_group, criterion_main};
 
 fn benchmarks(c: &mut Criterion) {
     let pysrc = PathBuf::from("./tests/fixtures/basic/src.py");
     let mut pt_cmd = cargo_bin_cmd!("pt");
     pt_cmd.arg(pysrc.as_os_str());
+    let mut pytest_cmd = Command::new("pytest");
+    pytest_cmd.arg(pysrc.as_os_str());
     c.bench_function("pt", |b| b.iter(|| pt_cmd.output()));
+    c.bench_function("pytest", |b| b.iter(|| pt_cmd.output()));
 }
 
 criterion_group!(basic, benchmarks);
